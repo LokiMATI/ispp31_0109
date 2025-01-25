a840b2e9a8be28f14a6b426096248cca7f433771
GSEvgenievich <Stepan.geras2006@yandex.ru>
2025-01-25 13:22:27 +0300

    LabWork5

---
+++ LabWork5/Task 1/Applicant.cs
@@ -0,0 +1,50 @@
+﻿public class Applicant
+{
+    private string? _fullName;
+    private int _classNumber;
+    private double _gpa;
+
+    public Applicant() : this("Unknown", 1, 1)
+    {
+    }
+
+    public Applicant(string fullName, int classNumber, double gpa)
+    {
+        FullName = fullName;
+        ClassNumber = classNumber;
+        GPA = gpa;
+    }
+
+    public string FullName
+    {
+        get => _fullName;
+        set
+        {
+            if (value.Trim() != String.Empty)
+                _fullName = value;
+        }
+    }
+
+    public int ClassNumber
+    {
+        get => _classNumber;
+        set
+        {
+            if (value > 0 && value <= 11)
+                _classNumber = value;
+        }
+    }
+
+    public double GPA
+    {
+        get => _gpa;
+        set
+        {
+            if (value > 0 && value <= 5.0)
+                _gpa = value;
+        }
+    }
+
+    public void Print()
+        => Console.WriteLine($"Абитуриент: {_fullName} закончил {_classNumber} классов со средним балом {_gpa}");
+}
---
+++ LabWork5/Task 1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задание 1
+
+Applicant applicant1 = new();
+Applicant applicant2 = new("Матигоров Никита Иванови", 9, 4.54);
+
+// Задание 2
+
+applicant1.Print();
+applicant2.Print();
+Console.WriteLine();
+
+// Задание 3
+
+applicant1.FullName = "  ";
+applicant2.ClassNumber = 12;
+applicant2.GPA = -12.0;
+
+applicant1.Print();
+applicant2.Print();
+Console.WriteLine();
+
+applicant1.FullName = "Тестов Тест Тестирович";
+applicant2.ClassNumber = 11;
+applicant2.GPA = 5;
+
+applicant1.Print();
+applicant2.Print();
+
+Applicant[] applicants = {
+    new("Приветов Привет Приветович", 11, 4.89),
+    new("Запоздалов Виктор Проспатович", 8, 3.66),
+    applicant1,
+    applicant2
+};
+
+Console.WriteLine("\nа) Окончившие 11 классов:");
+foreach (var applicant in applicants)
+    if (applicant.ClassNumber == 11)
+        applicant.Print();
+
+Console.Write("\nВведите средний бал: ");
+double gpa = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("\nб)  Абитуриенты со средним баллом выше заданного:");
+foreach (var applicant in applicants)
+    if (applicant.GPA > gpa)
+        applicant.Print();
