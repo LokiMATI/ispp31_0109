88251ec73c79d7215f5aafc4392f2bf23be6b973
Nikita Matigorov <r.matigorov@yandex.ru>
2025-01-29 00:13:01 +0300

    LabWork6 complited

--- LabWork6/LabWork/Applicant.cs
+++ LabWork6/LabWork/Applicant.cs
@@ -1,6 +1,8 @@
-﻿public class Applicant
+﻿namespace LabWork;
+
+public class Applicant
 {
-    private string? _fullName;
+    private string _fullName;
     private int _classNumber;
     private double _averageScore;
 
@@ -10,41 +12,10 @@
 
     public Applicant(string fullName, int classNumber, double averageScore)
     {
-        FullName = fullName;
-        ClassNumber = classNumber;
-        AverageScore = averageScore;
+        _fullName = fullName;
+        _classNumber = classNumber;
+        _averageScore = averageScore;
     }
-
-    public string FullName
-    {
-        get => _fullName;
-        set
-        {
-            if (value.Trim() != String.Empty)
-                _fullName = value;
-        }
-    }
-
-    public int ClassNumber
-    {
-        get => _classNumber;
-        set
-        {
-            if (value > 0 && value <= 11)
-                _classNumber = value;
-        }
-    }
-
-    public double AverageScore
-    {
-        get => _averageScore;
-        set
-        {
-            if (value > 0 && value <= 5.0)
-                _averageScore = value;
-        }
-    }
-
     public object this[string index]
     {
         get
@@ -73,7 +44,4 @@
             return '\0';
         }
     }
-
-    public void Print()
-        => Console.WriteLine($"Абитуриент: {_fullName} закончил {_classNumber} классов со средним балом {_averageScore}");
 }
--- LabWork6/LabWork/Program.cs
+++ LabWork6/LabWork/Program.cs
@@ -1,3 +1,47 @@
-﻿Applicant applicant = new();
+﻿using LabWork;
 
-Console.WriteLine($"{applicant[0]} a");
+class Program
+{
+    static void Main()
+    {
+        // Задание 1
+        Applicant applicant1 = new Applicant();
+        Applicant applicant2 = new Applicant("Матигоров Никита Иванович", 9, 4.44);
+
+        // Задание 2
+        Console.WriteLine(applicant2[2]);
+        Console.WriteLine(applicant2[-1]);
+        Console.WriteLine(applicant1["ФИО"]);
+
+        // Задание 3
+        Console.WriteLine(GetPerimeter(1));
+        Console.WriteLine(GetPerimeter(1, 2));
+
+        // Задание 4
+        Console.WriteLine(GetDegree(2, 3));
+        Console.WriteLine(GetDegree(2));
+        
+    }
+
+    static int GetPerimeter(int side) =>
+        side * 4;
+
+    static int GetPerimeter(int height, int width) =>
+        2 * (height + width);
+
+    static int GetDegree(int x, int n)
+    {
+        if (n < 0)
+            return -1;
+
+        int result = 1;
+
+        for (int i = n; i > 0; i--)
+            result *= x;
+        
+        return result;
+    }
+
+    static int GetDegree(int x) =>
+        x * x;
+}
