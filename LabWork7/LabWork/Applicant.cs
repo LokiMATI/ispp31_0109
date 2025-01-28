59f34d13a54a04abe159556e58c5ceb814883e59
Nikita Matigorov <r.matigorov@yandex.ru>
2025-01-29 00:36:35 +0300

    LabWork7 Task 1

---
+++ LabWork7/LabWork/Applicant.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace LabWork;
+
+public class Applicant
+{
+    public string FullName {  get; set; }
+    public int ClassNumber { get; set; }
+    public double AverageScore { get; set; }
+
+    public void Print() =>
+        Console.WriteLine($"ФИО: {FullName}; Выпускной класс: {ClassNumber}; Средний балл: {AverageScore}");
+
+    public static Applicant operator ++(Applicant applicant) => 
+        new Applicant { 
+        FullName = applicant.FullName, 
+        ClassNumber = applicant.ClassNumber, 
+        AverageScore = applicant.AverageScore++
+    };
+}
+
---
+++ LabWork7/LabWork/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LabWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Applicant applicant = new Applicant() { FullName = "Матигоров Никита Иванович", ClassNumber = 9, AverageScore = 3.44 };
+
+
+            // Задание 1
+            applicant.Print();
+            applicant++;
+            applicant.Print();
+
+
+        }
+    }
+}
