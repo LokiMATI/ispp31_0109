f2e4cbf83ca490f8c5e6bc7f1cf4b32e00fcd9d6
GSEvgenievich <Stepan.geras2006@yandex.ru>
2025-01-30 09:16:12 +0300

    Lection 0130

---
+++ Lections0101/Lection0130/Factory.cs
@@ -0,0 +1,38 @@
+﻿public class Factory
+{
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Прибыль
+    /// </summary>  
+    public int Income { get; set; }
+
+    public void ShowInfo()
+        => Console.WriteLine($"{Name}, {Income} руб.");
+
+    public static Factory operator ++(Factory factory) => 
+        new() 
+        { 
+            Name = factory.Name, 
+            Income = factory.Income + 1000 
+        };
+
+    public static Factory operator +(Factory factoryParent, Factory factoryChild) =>
+        new()
+        {
+            Name = factoryParent.Name,
+            Income = factoryParent.Income + factoryChild.Income
+        };
+
+    public static bool operator true(Factory factory)
+    {
+        return factory.Income > 0;
+    }
+
+    public static bool operator false(Factory factory)
+    {
+        return factory.Income <= 0;
+    }
+}
---
+++ Lections0101/Lection0130/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("operations\n");
+
+Factory factory = new() { Name = "SEVMASH", Income = 1000000 };
+factory.ShowInfo();
+factory++;
+factory.ShowInfo();
+
+Factory starFactory = new() { Name = "Zvezdochka", Income = 500000 };
+
+Factory Severodvinsk = factory + starFactory;
+Severodvinsk.ShowInfo();
