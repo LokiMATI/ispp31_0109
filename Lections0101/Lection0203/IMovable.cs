54184b27809c6be00c4c5a659c4c41a42b11f05e
Waxmater <01eg.4ursanov@gmail.com>
2025-02-03 13:32:33 +0300

    Lection0203

---
+++ Lections0101/Lection0203/IMovable.cs
@@ -0,0 +1,7 @@
+﻿public interface IMovable
+{
+    const int MinSpeed = 0;
+    static int MazSpeed = 60;
+    void Move();
+    string SpeedInfo {  get; }
+}
---
+++ Lections0101/Lection0203/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("interface");
+
+//Car car = new() { Brand = "bmw" };
+
+//class Car : IEquatable<Car>
+//{
+//    public string Brand { get; set; }
+//    public int Speed { get; set; }
+
+//    public bool Equals(Car? other)
+//        => Brand == other?.Brand && Speed == other?.Speed;
+//}
+
+Car car = new() { Brand = "bmw" };
+Car[] cars = [
+        new() {Brand = "bmw", Speed = 120},
+        new() {Brand = "lada", Speed = 60},
+        new() {Brand = "toyota", Speed = 200},
+   ];
+
+foreach (var car in cars)
+    Console.WriteLine(car);
+
+Console.WriteLine();
+
+Array.Sort(cars);
+foreach (var car in cars)
+    Console.WriteLine(car);
+class Car1 : IEquatable<Car>
+{
+    public string Brend { get; set; }
+    public int Speed  { get; set; }
+
+    public bool Equals(Car? other) 
+        => Brend == other?.Brand && Speed == other?.Speed;
+}
+class Car : IComparable //CompareTo/Compare
+{
+    public string Brand { set; get; }
+    public int Speed { get; set; }
+
+    //public int CompareTo(object? obj)
+    //{
+    //    if (obj is Car car)
+    //        return Speed.CompareTo(car.Speed);
+    //    throw new ArgumentException();
+    //    //var car = obj as Car;
+    //    //return Speed.CompareTo(car?.Speed);
+    //    /*if (Speed > car.Speed)
+    //        return 1;
+    //    if (Speed < car.Speed ) 
+    //        return -1;
+    //    return 0;*/
+    //}
+    public override string ToString()
+    {
+        return $"{Brand}, {Speed} км/ч";
+    }
+
+    int IComparable.CompareTo(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+}
