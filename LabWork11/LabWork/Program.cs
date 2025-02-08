377a00f2394a73333470b5dce61861c0fb45801e
Waxmater <01eg.4ursanov@gmail.com>
2025-02-08 11:50:46 +0300

    LabWork11

---
+++ LabWork11/LabWork/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задание 1
+List<string> flowers = ["Мак", "Роза", "Ромашка"];
+
+Console.Write("Введите количество новых элементов: ");
+int count = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значения для новых элементов:");
+for (int i = 0; i < count; i++)
+    flowers.Add(Console.ReadLine());
+
+for (int i = 0; i < flowers.Count; i++)
+    Console.WriteLine($"{i + 1}: {flowers[i]}");
+
+// Задание 4
+Console.Write("\nВведите значение элемента: ");
+string deleteValue = Console.ReadLine();
+
+flowers.RemoveAll(f => f.ToLower() == deleteValue.ToLower());
+
+foreach (var flower in flowers)
+    Console.WriteLine(flower);
---
+++ LabWork11/Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задание 2
+Dictionary<string, double> currencies = new()
+{
+    ["Доллар"] = 0.5,
+    ["Евро"] = 0.8,
+    ["Юань"] = 1000000
+};
+
+Console.Write("Введите количество новых элементов: ");
+int count = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите значения для новых валют:");
+for (int i = 0; i < count; i++)
+{
+    Console.Write("Введите название валюты: ");
+    var currencyName = Console.ReadLine();
+
+    Console.Write("Введите курс валюты относительно рубля: ");
+    var currencyValue = double.Parse(Console.ReadLine());
+
+    currencies[currencyName] = currencyValue;
+    Console.WriteLine();
+}
+
+foreach (var currency in currencies)
+    Console.WriteLine($"{currency.Key} - {currency.Value}");
+
+Console.WriteLine($"Количество элементов в словаре: {currencies.Count}");
+
+// Задание 3
+// 1
+Console.Write("\nВведите ключ: ");
+string key = Console.ReadLine();
+
+if (currencies.ContainsKey(key))
+    Console.WriteLine($"{key} - {currencies[key]}");
+else
+    Console.WriteLine("Такого ключа нет в словаре.");
+
+// 2
+Console.Write("\nВведите значение: ");
+double currencyValueInput = double.Parse(Console.ReadLine());
+
+int countValue = 0;
+
+foreach (var value in currencies.Values)
+    if (value == currencyValueInput)
+        countValue++;
+
+Console.WriteLine($"Количество значений в словаре равно: {countValue}");
+
+// 3
+Console.Write("Введите ключ для удаления элемента: ");
+var deleteKey = Console.ReadLine();
+
+currencies.Remove(deleteKey);
+
+foreach (var currency in currencies)
+    Console.WriteLine($"{currency.Key} - {currency.Value}");
