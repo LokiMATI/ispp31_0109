1d369be963a279e520f3153c1845a2b1ea3f843e
Waxmater <01eg.4ursanov@gmail.com>
2025-02-11 10:07:21 +0300

    Lec0208_2

--- Lections0101/Lection0208_2/Program.cs
+++ Lections0101/Lection0208_2/Program.cs
@@ -1,4 +1,26 @@
-﻿Console.WriteLine("struct");
+﻿using System.Text.RegularExpressions;
+
+var point = (2, 5);
+point.Item1 = 68;
+
+//var point = (x: 2, y: 5);
+//point.Item1 = 4;
+//point.x = 75;
+Console.WriteLine(point);
+
+(double, double, double) point3d = (0, 3, 5);
+
+(int, string) user = (Id: 123, Login: "admin");
+Console.WriteLine(user);
+
+SummerMonth month = SummerMonth.Jule;
+Color purple = Color.Red | Color.Blue;
+
+Season season = (Season)3;      // Season.Winter
+int monthNumber = (int)month;   // 7
+int purpleValue = (int)purple;  // 1+4=5
+
+RegexOptions options = RegexOptions.Multiline | RegexOptions.IgnoreCase | RegexOptions.RightToLeft;
 
 for (int i = 0; i < 16; i++)
 {
@@ -9,12 +31,10 @@ for (int i = 0; i < 16; i++)
 
 Person person1 = new() { Id = 123, FullName = "qwe asd zxc" };
 Console.WriteLine(person1);
-Person person2 = new() { Id = 123, FullName = "qwe asd zxc" };
+Person person2 = new() { Id = 1, FullName = "qwe asd zxc" };
 Console.WriteLine(person2);
 
 if (person1.Equals(person2))
     Console.WriteLine("равны");
 else
     Console.WriteLine("не равны");
-
-Point2d point = new();
