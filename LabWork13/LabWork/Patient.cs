61581a48ce3630e217047ae557cdf4ec3e43e414
Nikita Matigorov <r.matigorov@yandex.ru>
2025-02-26 06:11:04 +0300

    LabWork13 часть заданий

---
+++ LabWork13/LabWork/Patient.cs
@@ -0,0 +1,11 @@
+﻿public class Patient
+{
+    public string FullName { get; set; }
+    public string Polis { get; set; }
+    public DateTime BirthDate { get; set; }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
