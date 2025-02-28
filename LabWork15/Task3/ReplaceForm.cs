c16c9b5c8d2f8acb72768c41c3f2018526845cba
LokiMATI <r.matigorov@yandex.ru>
2025-02-28 10:10:19 +0300

    LabWork15

---
+++ LabWork15/Task3/ReplaceForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class ReplaceForm : Form
+    {
+        public Action<string, string> ReplaceText;
+
+        public ReplaceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ReplaceButton_Click(object sender, EventArgs e)
+        {
+            ReplaceText?.Invoke(SearchTextBox.Text, ReplaceTextBox.Text);
+        }
+    }
+}
