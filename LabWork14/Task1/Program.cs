7703a3dad21fc73b1a78a421905d24e5e2590f83
LokiMATI <r.matigorov@yandex.ru>
2025-03-07 11:44:37 +0300

    LabWork14

---
+++ LabWork14/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задание 1
+using System.Text.RegularExpressions;
+
+Console.Write("Введите номер телефона: ");
+string number = Console.ReadLine();
+
+Regex regex = new(@"(\+7|8)\(9[0-9]{2}\)[0-9]{3}\-[0-9]{2}\-[0-9]{2}");
+
+if (regex.IsMatch(number))
+    Console.WriteLine("Введенный номер является корректным.");
+else
+    Console.WriteLine("Введенный номер не является корректным.");
