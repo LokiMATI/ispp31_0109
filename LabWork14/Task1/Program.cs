e52db42779ae383b9a42f5fb4cc5741889fdd507
Nikita Matigorov <r.matigorov@yandex.ru>
2025-03-11 20:27:59 +0300

    LabWork14Home

---
+++ LabWork14/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание 1
+
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+string pattern = @"^(\+7|8)\(9\d{2}\)\d{3}-\d{2}-\d{2}$";
+Console.WriteLine("Введите номер телефона, чтобы соответствовал формату +7(9хх)ххх-хх-хх или 8(9хх)ххх-хх-хх, где х — любая цифра:");
+string numberPhone = Console.ReadLine();
+
+if (Regex.IsMatch(numberPhone, pattern))
+    Console.WriteLine("Введёный номер телефона соответствует формату.");
+else
+    Console.WriteLine("Введёный номен телефона НЕ соответствует формату.");
---
+++ LabWork14/Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задание 2
+using System.Text.RegularExpressions;
+
+Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+string pattern = @"\s+";
+string replacement = " ";
+
+Console.WriteLine(Regex.Replace(input, pattern, replacement));
