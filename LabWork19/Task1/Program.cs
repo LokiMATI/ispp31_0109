02f74dcb758856feb84c55138ce6a21f42d708e4
LokiMATI <r.matigorov@yandex.ru>
2025-03-14 12:00:28 +0300

    labwork19

---
+++ LabWork19/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задание 1
+using Task1;
+
+int num1 = 3, num2 = 2;
+
+Context context = new(new OperationAdd());
+Console.WriteLine($"OpearationAdd: {context.ExecuteStrategy(num1, num2)}");
+
+context = new(new OperationSubstract());
+Console.WriteLine($"OperationSubstract: {context.ExecuteStrategy(num1, num2)}");
+
+context = new(new OperationMultiply());
+Console.WriteLine($"OperationMultiply: {context.ExecuteStrategy(num1, num2)}");
+
+context = new(new OperationDivide());
+Console.WriteLine($"OperationDivision: {context.ExecuteStrategy(num1, num2)}");
---
+++ LabWork19/Task5/Confihuration.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace Task5
+{
+    public class Configuration
+    {
+        private static readonly Configuration _instance = new();
+
+        public Dictionary<string, string> settings = new Dictionary<string, string>();
+
+        private Configuration() { }
+
+        public static Configuration GetInstance() => _instance;
+
+        public void RemoveSetting(string index)
+            => settings.Remove(index);
+
+        public Configuration? this[string index]
+        {
+            get => _instance[index];
+            set => _instance[index] = value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var setting in settings)
+            {
+                builder.AppendLine($""""""")
+            }
+        }
+    }
+}
