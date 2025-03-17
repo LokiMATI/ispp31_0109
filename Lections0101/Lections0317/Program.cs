582ebac5d8b1c24e1e62b354112280179240f130
LokiMATI <r.matigorov@yandex.ru>
2025-03-17 15:10:21 +0300

    lectoin0317

---
+++ Lections0101/Lections0317/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("MV(x)");
+
+User user = GetUserById(123);
+
+UserView view = new();
+
+UserController controller = new(user, view);
+controller.UpdateView();
+controller.UserLogin = "adm";
+controller.UpdateView();
+
+static User GetUserById(int id)
+    => new() { Id = id, Login = "admin" };
