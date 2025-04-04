2268233a09c0005f4e7c78a1ec8f9bbb1b5be621
user2 <user2@example.com>
2025-04-04 08:13:39 +0300

    LabWork14

--- LabWork14/Task3/Program.cs
+++ LabWork14/Task3/Program.cs
@@ -1,1 +1,11 @@
 ﻿// Задание 3
+using System.Text.RegularExpressions;
+
+Regex regex = new(@"^[a-z1-9_\-\.]+\@([a-z0-9]+\.){0,2}[a-z]+", RegexOptions.IgnoreCase);
+Console.Write("Введите адрес электронной почты: ");
+string input = Console.ReadLine();
+
+if (regex.IsMatch(input))
+    Console.WriteLine("Введён верный адрес электронной почты.");
+else
+    Console.WriteLine("Введён неверный адрес электронной почты.");
---
+++ LabWork14/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задание 5
+using System.Text.RegularExpressions;
+
+Regex regex = new(@"^\d{2}[./]\d{2}[./]\d{4}$");
+Console.Write("Введите дату: ");
+string input = Console.ReadLine();
+
+if (!regex.IsMatch(input))
+{
+    Console.WriteLine("Некорректный формат ввода даты.");
+    return;
+}
+
+Regex pattern = new(@"^(?<day>\d{2})[./](?<month>\d{2})[./](?<year>\d{4})$");
+
+string formatedDate = pattern.Replace(input, "${year}-${month}-${day}");
+
+Console.WriteLine(formatedDate);
+
+    
