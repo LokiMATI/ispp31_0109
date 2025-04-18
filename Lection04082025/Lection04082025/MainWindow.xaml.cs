68a6b460789ee5867bbe673cf220f95e73ca52f9
user2 <user2@example.com>
2025-04-18 10:12:41 +0300

    Lections WTF

---
+++ Lection04082025/Lection04082025/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lection04082025.Pages;
+
+namespace Lection04082025
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            App.MainFrame = ContentFrame;
+            App.MainFrame.Navigate(new StartPage());
+        }
+
+        private void Page1Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.MainFrame.Navigate(new Page1());
+        }
+        private void Page2Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.MainFrame.Navigate(new Page2());
+        }
+        private void Page3Button_Click(object sender, RoutedEventArgs e)
+        {
+            App.MainFrame.Navigate(new Page3());
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.MainFrame.CanGoBack)
+                App.MainFrame.GoBack();
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (App.MainFrame.CanGoForward)
+                App.MainFrame.GoForward();
+        }
+
+        private void ContentFrame_ContentRendered(object sender, EventArgs e)
+        {
+            BackButton.IsEnabled = App.MainFrame.CanGoBack;
+            ForwardButton.IsEnabled = App.MainFrame.CanGoForward;
+            //if (App.MainFrame.CanGoBack)
+            //    BackButton.Visibility = Visibility.Visible;
+            //else
+            //    BackButton.Visibility = Visibility.Collapsed;
+
+            //ForwardButton.Visibility = App.MainFrame.CanGoForward ? 
+            //    Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ Lection04082025/Lection04082025/Pages/StartPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lection04082025.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для StartPage.xaml
+    /// </summary>
+    public partial class StartPage : Page
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+
+            StackPanel panel = new();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Label label = new();
+                label.Content = $"-{i + 1} text";
+                panel.Children.Add(label);
+            }
+            Content = panel;
+        }
+    }
+}
+
