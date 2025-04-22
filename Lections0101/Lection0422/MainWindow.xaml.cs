8af51d6dcbd25cf57aaf585d8c0db485f5c7e7d7
user2 <user2@example.com>
2025-04-22 10:10:22 +0300

    Lection 0422

---
+++ Lections0101/Lection0422/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Lection0422
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LightButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme("LightTheme");
+        }
+
+        private void DarkButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeTheme("DarkTheme");
+        }
+
+        private void ChangeTheme(string name)
+        {
+            Application.Current.Resources.MergedDictionaries.Clear();
+
+            AddTheme(name);
+            AddTheme("ControlsTheme");
+        }
+
+        private static void AddTheme(string name)
+        {
+            var uriDictionary = new Uri($"Themes/{name}.xaml", UriKind.Relative);
+            var dictionary = Application.LoadComponent(uriDictionary) as ResourceDictionary;
+            Application.Current.Resources.MergedDictionaries.Add(dictionary);
+        }
+    }
+}
