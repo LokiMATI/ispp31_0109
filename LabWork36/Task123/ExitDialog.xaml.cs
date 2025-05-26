d809f6cfd5fe0f9e919a3a372ed7c9d5902e005b
0109-13 <0109-13@0109-13>
2025-05-26 16:56:21 +0300

    LabWork36

---
+++ LabWork36/Task123/ExitDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Task123
+{
+    /// <summary>
+    /// Логика взаимодействия для ExitDialog.xaml
+    /// </summary>
+    public partial class ExitDialog : Window
+    {
+        public ExitDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
---
+++ LabWork36/Task123/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task123
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewWindowMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = new();
+            window.Show();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            ExitDialog dialog = new();
+
+            if (dialog.ShowDialog() != true)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
