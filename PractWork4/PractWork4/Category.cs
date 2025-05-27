41a3f2a79ceb5d2dc813f861716436597c255f0c
0109-13 <0109-13@0109-13>
2025-05-27 16:09:24 +0300

    PractWork4

---
+++ PractWork4/PractWork4/Category.cs
@@ -0,0 +1,7 @@
+﻿namespace PractWork4;
+
+public class Category
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ PractWork4/PractWork4/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace PractWork4;
+
+public class Game
+{
+    public int IdGame { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? Category { get; set; }
+    public double Price { get; set; }
+    public string PriceInfo { get
+        {
+            if (Sale > 0)
+                return $"~~{Price}~~₽ {Price * (Sale / 100)}₽";
+            return $"{Price}₽";
+        }
+    }
+    public double Sale { get; set; } = 0;
+}
