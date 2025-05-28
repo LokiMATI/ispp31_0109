b4935dcca896ae9934cb2f8e6d7ab298456ec1c5
0109-13 <0109-13@0109-13>
2025-05-28 10:10:00 +0300

    Lection0527/28

---
+++ Lections0101/Lection0527/CatsWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Lection0527
+{
+    /// <summary>
+    /// Логика взаимодействия для CatsWindow.xaml
+    /// </summary>
+    public partial class CatsWindow : Window
+    {
+        
+
+        public CatsWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
---
+++ Lections0101/Lection0528/Program.cs
@@ -0,0 +1,50 @@
+﻿using Lection0528;
+using System.Configuration;
+using System.Text.Json;
+
+Console.WriteLine("settings");
+/*
+// txt
+string fileName = "settings.txt";
+var txt = File.ReadAllText(fileName);
+
+// csv
+fileName = "settings.csv";
+var csv = File.ReadAllLines(fileName);
+foreach (var line in csv)
+{
+    var cells = line.Split(';');
+    var login = cells[0];
+    var password = cells[1];
+    var age = int.Parse(cells[2]);
+}
+
+// xml
+*/
+// json
+string fileName = "Users.json";
+string json = File.ReadAllText(fileName);
+
+JsonSerializerOptions options = new()
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+
+var users = JsonSerializer.Deserialize<List<User>>(json, options);
+
+options = new()
+{
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+
+json = JsonSerializer.Serialize<List<User>>(users, options);
+
+var login = ConfigurationManager.AppSettings["Login"];
+var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+config.AppSettings.Settings["Login"].Value = "Менеджер";
+config.Save();
+ConfigurationManager.RefreshSection("appSettings");
+Console.WriteLine();
+
+
