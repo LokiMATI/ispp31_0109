23566594950e796b02bcee611fe1cf24c1d213d6
Lucky <r.matigorov@yandex.ru>
2025-06-16 08:31:21 +0300

    LabWork41 Task1

---
+++ LabWork41/Task1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new();
+            animation.From = Menu.Width;
+            animation.To = Menu.Width > 0 ? 0 : 80;
+            animation.Duration = TimeSpan.FromSeconds(1);
+            Menu.BeginAnimation(Menu.WidthProperty, animation);
+        }
+    }
+}
